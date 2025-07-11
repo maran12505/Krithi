608760fbd500be888c37f9f1cdfdb8e0bc1663a0
maran12505 <madhumaran.manivannan@integra.co.in>
2025-07-11 19:40:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using WriteUp.Components;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
+
+
+
+// This is a simple ASP.NET Core application that uses Razor Components to create an interactive web application.
+
